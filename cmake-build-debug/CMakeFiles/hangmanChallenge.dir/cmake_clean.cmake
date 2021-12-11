e6f40c8199ff876402776fa237a26973a19a9c9e
KeenanRileyFaulkner <95945751+KeenanRileyFaulkner@users.noreply.github.com>
2021-12-10 21:11:51 -0700

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/hangmanChallenge.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hangmanChallenge.dir/main.cpp.obj"
+  "hangmanChallenge.exe"
+  "hangmanChallenge.exe.manifest"
+  "hangmanChallenge.pdb"
+  "libhangmanChallenge.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hangmanChallenge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
